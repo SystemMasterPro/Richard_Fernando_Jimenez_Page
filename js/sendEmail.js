3799cb371c830f2abb2720381107048c061dcab5
Richard <richardjimenez.9641@gmail.com>
2021-11-16 12:57:11 -0500

    Send Email

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,9 +1,34 @@
 const send = document.getElementById("sendEmail");
+const form = document.getElementById("MyFormEmail");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
 send.addEventListener('click', function () {
     event.preventDefault();
-    Swal.fire({
-        icon: 'warning',
-        title: 'Tranquilo...',
-        text: 'Estamos trabajando en ello! De momento puedes conectar conmigo por las otras opciones disponibles'
-    });
+    if (name.value === "" || email.value === "" || message.value === "") {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Upss...',
+            text: 'Se requiere llenar todos los campos!!!'
+        });
+    } else {
+        emailjs.sendForm('gmailMessage', 'template_ysv8ybn', 'form', 'user_ji7M3KpHpxSg2fvS2O20z')
+            .then(function (response) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'CORREO ENVIADO CON EXITO!',
+                    text: 'Su correo ha sido entregado de manera satisfactoria, nos pondremos en contacto de inmediato.'
+                });
+                name.value = "";
+                email.value = "";
+                message.value = "";
+            }, function (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Upss!',
+                    text: 'Hemos tenido problemas al enviar su correo, verifique su conexion y vuelva a intentarlo.'
+                });
+            });
+    }
 });
